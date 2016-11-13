fa231d464edd9fef78f91fed23065178b512fce3
Jobin Johnson <jobinrjohnson@gmail.com>
2016-11-13 13:15:39 +0530

    updated4

--- assets/bootstrap/formvalidate.js
+++ assets/bootstrap/formvalidate.js
@@ -152,20 +152,70 @@ function validateEmail(email) {
 function adjustui() {
     if (window.innerWidth > 720) {
         if (window.scrollY >= window.innerHeight)
-            $(".defnav").css("background", "#0f0044");
+            $(".defnav").css("background", "#05b5bd");
         else
             $(".defnav").css("background", "none");
     }
 }
 function adjustui2() {
     if (window.innerWidth > 720) {
-        $(".defnav").css("background", "#0f0044");
+        $(".defnav").css("background", "#05b5bd");
         $(".defnav").removeClass("navbar-default");
         $(".defnav").addClass("navbar-inverse")
     } else {
-        $(".defnav").css("background", "#0f0044");
+        $(".defnav").css("background", "#05b5bd");
         $(".defnav").addClass("navbar-inverse");
     }
 }
 window.onresize = adjustui2;
 window.onscroll = adjustui;
+
+
+
+
+
+
+
+
+
+function getTimeRemaining(endtime) {
+  var t = Date.parse(endtime) - Date.parse(new Date());
+  var seconds = Math.floor((t / 1000) % 60);
+  var minutes = Math.floor((t / 1000 / 60) % 60);
+  var hours = Math.floor((t / (1000 * 60 * 60)) % 24);
+  var days = Math.floor(t / (1000 * 60 * 60 * 24));
+  return {
+    'total': t,
+    'days': days,
+    'hours': hours,
+    'minutes': minutes,
+    'seconds': seconds
+  };
+}
+
+function initializeClock(id, endtime) {
+  var clock = document.getElementById(id);
+  var daysSpan = clock.querySelector('.days');
+  var hoursSpan = clock.querySelector('.hours');
+  var minutesSpan = clock.querySelector('.minutes');
+  var secondsSpan = clock.querySelector('.seconds');
+
+  function updateClock() {
+    var t = getTimeRemaining(endtime);
+
+    daysSpan.innerHTML = t.days;
+    hoursSpan.innerHTML = ('0' + t.hours).slice(-2);
+    minutesSpan.innerHTML = ('0' + t.minutes).slice(-2);
+    secondsSpan.innerHTML = ('0' + t.seconds).slice(-2);
+
+    if (t.total <= 0) {
+      clearInterval(timeinterval);
+    }
+  }
+
+  updateClock();
+  var timeinterval = setInterval(updateClock, 1000);
+}
+
+var deadline = new Date(Date.parse(new Date()) + 15 * 24 * 60 * 60 * 1000);
+initializeClock('clockdiv', deadline);
