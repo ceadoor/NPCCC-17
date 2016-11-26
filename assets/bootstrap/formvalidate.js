52e507fa160da429db854e494446014e7d7dbc83
Jobin Johnson <jobinrjohnson@gmail.com>
2016-11-26 09:23:53 +0100

    Update 'assets/bootstrap/formvalidate.js'

--- assets/bootstrap/formvalidate.js
+++ assets/bootstrap/formvalidate.js
@@ -217,5 +217,5 @@ function initializeClock(id, endtime) {
   var timeinterval = setInterval(updateClock, 1000);
 }
 
-var deadline = new Date(Date.parse(new Date()) + 15 * 24 * 60 * 60 * 1000);
+var deadline = new Date(Date.parse("01/01/2017"));
 initializeClock('clockdiv', deadline);
